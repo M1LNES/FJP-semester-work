a24101aa6f7b327139c49964da6fc4d429ce31f0
Jakub Pavlíček <kuba.cb@seznam.cz>
2024-12-19 00:11:02 +0100

    include ANTLR in the JAR, use .ligma file to define program, use run.sh to start the compiler

--- ligma/src/main/java/ligma/App.java
+++ ligma/src/main/java/ligma/App.java
@@ -1,7 +1,46 @@
 package ligma;
 
+import ligma.generated.LigmaLexer;
+import ligma.generated.LigmaParser;
+import ligma.visitor.ProgramVisitor;
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j2;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Log
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        if (args.length != 1) {
+            log.severe("Compiler expected one argument: file with program");
+            return;
+        }
+
+        String filename = args[0];
+
+        try(InputStream input = new FileInputStream(filename))
+        {
+            log.info("Successfully opened file: " + filename);
+
+            CharStream charStream = CharStreams.fromStream(input);
+            LigmaLexer ligmaLexer = new LigmaLexer(charStream);
+            CommonTokenStream tokenStream = new CommonTokenStream(ligmaLexer);
+            LigmaParser parser = new LigmaParser(tokenStream);
+            LigmaParser.ProgramContext programContext = parser.program();
+            ProgramVisitor programVisitor = new ProgramVisitor();
+
+            programVisitor.visit(programContext);
+        }
+        catch (IOException exception)
+        {
+            log.severe("File not found: " + filename);
+        }
     }
 }
